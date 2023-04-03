ece4180dc03391337bc9e5b6ab02fbbc9cb6291f
PHUTTAKAN POOLKHUN <129069064+idpb151508@users.noreply.github.com>
2023-04-03 21:08:42 +0700

    Add files via upload

---
+++ pinglinux.py
@@ -0,0 +1,40 @@
+import subprocess
+from time import sleep
+import requests
+
+ip = "192.168.1.6"
+
+def notifyNetwork(message):
+    token = "bvtX6Va3vJ5qs7lf79o0I6Yn81c1il9SPw7Il5MlsSr"
+    uri = "https://notify-api.line.me/api/notify"
+    header = {"Authorization": "Bearer "+token}
+    msg = {"message": message}
+    resp = requests.post(uri, headers=header, data=msg)
+
+
+def ping():
+    try:
+        subprocess.check_output(["ping", "-c", "1", ip])
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+previusStatus = True
+while True:
+    currentStatus = ping()  
+    if currentStatus == previusStatus:
+        print('-----no notify--------')
+    else:
+        if currentStatus:
+            statusNetwork = 'UP'
+            message = f"{ip} {statusNetwork}"
+            notifyNetwork(message)
+            print('--- Notify ----')
+        else:
+            statusNetwork = 'DOWN'
+            message = f"{ip} {statusNetwork}"
+            notifyNetwork(message)
+            print('--- Notify ----')
+    previusStatus = currentStatus
+    print(currentStatus)
+    sleep(2)
